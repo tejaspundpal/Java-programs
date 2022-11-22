7997747b334269306f40d940557d532d0aaa0f13
Tejas Pundpal <96496782+tejaspundpal@users.noreply.github.com>
2022-11-22 11:54:53 +0530

    Add files via upload

---
+++ college_programs/CountClickButton.java
@@ -0,0 +1,59 @@
+package college_programs;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+class CountClickButton extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					CountClickButton frame = new CountClickButton();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public CountClickButton() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Button Clicked 0 times.");
+		lblNewLabel.setBounds(10, 10, 176, 29);
+		contentPane.add(lblNewLabel);
+		
+		JButton btnNewButton = new JButton("Click Here");
+		btnNewButton.addActionListener(new ActionListener() {
+			int i=0;
+			public void actionPerformed(ActionEvent e) {
+				i++;
+				lblNewLabel.setText("Button Clicked "+i+" times.");
+			}
+		});
+		btnNewButton.setBounds(33, 57, 109, 36);
+		contentPane.add(btnNewButton);
+	}
+}
